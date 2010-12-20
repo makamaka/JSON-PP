34b9e0b1faa5ac335a214a473da32234ef5303ef
makamaka <makamaka.donzoko@gmail.com>
2010-12-20 20:31:15 +0900

    modified pod

--- lib/JSON/PP/Boolean.pm
+++ lib/JSON/PP/Boolean.pm
@@ -1,6 +1,6 @@
 =head1 NAME
 
-JSON::PPdev::Boolean - dummy module providing JSON::PP::Boolean
+JSON::PP::Boolean - dummy module providing JSON::PP::Boolean
 
 =head1 SYNOPSIS
 
@@ -9,7 +9,7 @@ JSON::PPdev::Boolean - dummy module providing JSON::PP::Boolean
 =head1 DESCRIPTION
 
 This module exists only to provide overload resolution for Storable and similar modules. See
-L<JSON::PPdev> for more info about this class.
+L<JSON::PP> for more info about this class.
 
 =cut
 
