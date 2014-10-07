30eb8691cde32a3519f9f5c97fa3dde760b22e4c
makamaka <makamaka.donzoko@gmail.com>
2014-10-07 14:59:16 +0900

    updated version for haarg's patch

--- lib/JSON/PP.pm
+++ lib/JSON/PP.pm
@@ -11,7 +11,7 @@ use Carp ();
 use B ();
 #use Devel::Peek;
 
-$JSON::PP::VERSION = '2.27203';
+$JSON::PP::VERSION = '2.27300';
 
 @JSON::PP::EXPORT = qw(encode_json decode_json from_json to_json);
 
@@ -1626,7 +1626,7 @@ JSON::PP - JSON::XS compatible pure-Perl module.
 
 =head1 VERSION
 
-    2.27202
+    2.27300
 
 L<JSON::XS> 2.27 (~2.30) compatible.
 
@@ -2787,7 +2787,7 @@ Makamaka Hannyaharamitu, E<lt>makamaka[at]cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2013 by Makamaka Hannyaharamitu
+Copyright 2007-2014 by Makamaka Hannyaharamitu
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
